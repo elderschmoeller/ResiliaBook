769c233147ffce9a610d08a95a8dd3c5fc18a7bd
Elder Schmoeller <elderschmoellerls@gmail.com>
2021-12-29 22:24:52 -0300

    FEAT: Adicionando primeiros arquivos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import "./index.css";
+
+import Header from "./components/Header/Header";
+import Button from "./components/Button/Button";
+import Cart from "./components/Cart/Cart";
+import Login from "./components/Login/Login";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+        <button className="init-btn">
+            Hello
+        </button>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+
+export default Header;
