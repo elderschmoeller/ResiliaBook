bca5e0dd7a0f8e2d60d81209a74fbd35e82c14f2
Vítor <vrgermano159@gmail.com>
2022-01-02 21:02:59 -0300

    Testando pagina livros

--- src/components/Livros/ListLivros.js
+++ src/components/Livros/ListLivros.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 export const ListLivro = () => {
+
     return (
-        <h1>Lista de Livros</h1>
-    )
-}
+        <div>
+            <h1>
+                Livros
+            </h1>
+            <ul id='Livros' style={{ listStyleType: 'none', margin: 0, padding: 0 }}>
+                <li>
+                    <h2>Nome: </h2>
+                    <h3>Editora: </h3>
+                </li>
+            </ul>
+        </div>
+    );
+};
 
 export default ListLivro;
