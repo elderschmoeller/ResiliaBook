149415e5e1782f847268a8adb2fe9709a6606992
Elder Schmoeller <elderschmoellerls@gmail.com>
2022-01-07 08:21:48 -0300

    FIX: modificando arquivos css, e linkando imagem svg

--- src/components/layouts/NavBar.js
+++ src/components/layouts/NavBar.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 import Container from "./Container";
 
 import styles from "../layouts/NavBar.module.css";
-import logo from "../../img/apple-touch-icon.png";
+import logo from "../../img/Resilia book.svg";
 
 function NavBar() {
   return (
-    <nav class={styles.navbar}>
+    <nav className={styles.navbar}>
       <Container>
         <Link>
-          <img src={logo} alt="ResiliaBook" />
+          <img className={styles.imgLogo} src={logo} alt="ResiliaBook" />
         </Link>
         <ul className={styles.list}>
           <li className={styles.item
