2280d7e3a98a9a7fe40b235b8bc4ceb13b826478
Elder Schmoeller <elderschmoellerls@gmail.com>
2022-01-07 08:22:45 -0300

    FEAT: adicionando arquivos HOME

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,15 @@
+import styles from './Home.module.css'
+import savings from '../../img/Resilia book.svg'
+import LinkButton from '../layouts/LinkButton'
+
 function Home(){
     return (
-    <h1>Home</h1>
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>Resilia Book</span></h1>
+            <p>Dê um start na sua leitura agora mesmo!</p>
+            <LinkButton to="/novolivro" text="Adicionar Livro" />
+            <img src={savings} alt='ResiliaBook'/>
+        </section>
     )
 }
 
