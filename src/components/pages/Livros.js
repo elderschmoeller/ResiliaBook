b5965994d29b42f6cabdbe0f28a417c6aac35492
Vítor <vrgermano159@gmail.com>
2022-01-08 18:16:00 -0300

    Apresentando requisicao na tela

--- src/components/pages/Livros.js
+++ src/components/pages/Livros.js
@@ -3,10 +3,13 @@ import axios from 'axios';
 
 function Livros(){
 
+    const  [ books, setBooks ] = useState([])
+
     useEffect(() => {
         axios.get('http://localhost:3005/livros')
         .then((response) => {
             console.log(response.data);
+            setBooks(response.data);
         })
         .catch(() => {
             console.log('A requisição deu errado!');
@@ -17,9 +20,19 @@ function Livros(){
 
     return (
     <div>
-        LIVROS
+        <ul id='livros' style={{ listStyleType: 'none', margin: 0, padding: 0 }}>
+          {books.map(livro => (
+            <li id={`livro${+livro.id}`}>
+              <h2>Titulo: {livro.titulo}</h2>
+              <h3>Autor: {livro.autor}</h3>
+              <h3>Editora: {livro.editora}</h3>
+              <h3>Preço: {livro.preco}</h3>
+              <br></br>
+            </li>
+          ))}
+        </ul>
     </div>
     )
 }
 
-export default Livros
+export default Livros;
