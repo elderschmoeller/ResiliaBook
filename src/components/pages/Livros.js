cf0d366edd7555e75e1b185961c3d1f984aa258b
Vítor <vrgermano159@gmail.com>
2022-01-08 17:42:57 -0300

    Requisicao valores API

--- src/components/pages/Livros.js
+++ src/components/pages/Livros.js
@@ -1,6 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function Livros(){
+
+    useEffect(() => {
+        axios.get('http://localhost:3005/livros')
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch(() => {
+            console.log('A requisição deu errado!');
+        })
+    }, [])
+
+
+
     return (
-    <h1>Livros</h1>
+    <div>
+        LIVROS
+    </div>
     )
 }
 
