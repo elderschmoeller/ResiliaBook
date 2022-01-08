1bdc931574296b438c05d05a60682c678242eed5
Vítor <vrgermano159@gmail.com>
2022-01-08 18:26:12 -0300

    criando página de edicao

--- src/components/Livros/AtualizaLivros.js
+++ src/components/Livros/AtualizaLivros.js
@@ -1,6 +1,4 @@
-import React, {useState} from 'react'
-import Input from '../Form/Input';
-import Select from '../Form/Select';
+import React from 'react'
 import SubmitButton from '../Form/SubmitButton';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
@@ -70,50 +68,5 @@ function LivrosForm({btnText}) {
     </div>
   )
 }
-
-/*
-function LivrosForm({btnText}) {
-  const [categories, setCategories] = useState([])
-
-  fetch("http://localhost:3005/categories", {
-    method: "GET",
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {setCategories(data)})
-    .catch((err) => console.log(err))
-  return (
-    <form className={styles.form}>
-      <Input
-        type="text"
-        text="Seu Nome Completo:"
-        name="name"
-        placeholder="Insira o seu Nome"
-      />
-      <Input
-        type="text"
-        text="Nome do Livro:"
-        name="name"
-        placeholder="Insira o Nome do Livro"
-      />
-      <Input
-        type="number"
-        text="Valor do Livro:"
-        name="name"
-        placeholder="Insira o valor do Livro"
-      />
-      <Select 
-      name="category_id" 
-      text="Selecione o Gênero do Livro:" 
-      options={categories}
-      />
-      <SubmitButton text={btnText} />
-    </form>
-  );
-}
-
-*/
 
 export default LivrosForm;
